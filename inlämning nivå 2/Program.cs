fa27599f53e1ba46f683f2418fbf10c7a34f4f83
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-14 20:26:27 +0100

    Add project files.

---
+++ inlämning nivå 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization.Json;
+namespace Inlämning_nivå_2
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            while (true)
+            {
+                Console.WriteLine("skriv in ett tal");
+                int tal  =int.Parse(Console.ReadLine());
+                if (tal == 0)
+                {
+                    break;
+                }
+                Console.WriteLine("programmet avslutas");
+                
+            }
+
+        }
+    }
+
+
+}
